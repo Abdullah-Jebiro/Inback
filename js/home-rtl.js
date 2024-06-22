4fb141fdede68e46ac2c12998e801a55c4a47e15
Abdullah Jebiro <a.jebiro@manteq-me.com>
2024-06-22 13:55:39 +0100

    Refactor: Add comments to jQuery code for service slider and FAQ functionality for better understanding

--- js/home-rtl.js
+++ js/home-rtl.js
@@ -1,46 +1,45 @@
-$(document).ready(function(){
-  // service slider
+$(document).ready(function() {
+  // Initialize the service slider using OwlCarousel
   $('.service-slider__content').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    autoplay: true,
-    rtl: true,
-    dots: false,
-    autoHeight: true,
-    lazyLoad:true,
+    loop: true, // Enable continuous looping
+    margin: 0, // Set margin between items to 0
+    nav: true, // Show navigation arrows
+    autoplay: true, // Enable autoplay
+    rtl: true, // Enable right-to-left mode
+    dots: false, // Disable dots navigation
+    autoHeight: true, // Adjust height automatically
+    lazyLoad: true, // Enable lazy loading of images
     navText: [
-      "<i class='fa fa-chevron-right slider-icon'></i>",
-      "<i class='fa fa-chevron-left slider-icon'></i>",
+      "<i class='fa fa-chevron-right slider-icon'></i>", // Custom right navigation arrow
+      "<i class='fa fa-chevron-left slider-icon'></i>" // Custom left navigation arrow
     ],
     responsive: {
       0: {
-        nav: false,
-        items: 1,
-        stagePadding: 20,
+        nav: false, // Hide navigation arrows on small screens
+        items: 1, // Show 1 item on small screens
+        stagePadding: 20 // Add padding on small screens
       },
       600: {
-        items: 1,
-        stagePadding: 20,
-        nav: false,
+        items: 1, // Show 1 item on medium screens
+        stagePadding: 20, // Add padding on medium screens
+        nav: false // Hide navigation arrows on medium screens
       },
       1000: {
-        items: 3,
-      },
-    },
+        items: 3 // Show 3 items on large screens
+      }
+    }
   });
 
-
-  // FAQ
-  $('.faq__tabs__tab').click(function (e) {
-		e.preventDefault();
-		$(this).addClass('active').siblings().removeClass('active');
-		const sectionShow = $(this).attr('data-target');
-		$(sectionShow)
-			.addClass('active')
-			.fadeIn()
-			.siblings()
-			.hide()
-			.removeClass('active');
-	});
-})
+  // Handle click events on FAQ tabs
+  $('.faq__tabs__tab').click(function(e) {
+    e.preventDefault(); // Prevent default anchor click behavior
+    $(this).addClass('active').siblings().removeClass('active'); // Set clicked tab as active and remove active class from siblings
+    const sectionShow = $(this).attr('data-target'); // Get the target section to show
+    $(sectionShow)
+      .addClass('active') // Set the target section as active
+      .fadeIn() // Fade in the target section
+      .siblings() // Get siblings of the target section
+      .hide() // Hide siblings
+      .removeClass('active'); // Remove active class from siblings
+  });
+});
